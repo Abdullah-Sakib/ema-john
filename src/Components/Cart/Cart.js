831a16826bc52a8179d2f278776c0d2da3b22b81
Abdullah-Sakib <abdullahsakib888@gmail.com>
2022-09-25 23:24:11 +0600

    add cart data to local storage

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -4,15 +4,23 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import './Cart.css'
 
 const Cart = ({cart}) => {
+  let price = 0;
+  let shipping = 0;
+  for(let product of cart){
+    price = price + product.price;
+    shipping = shipping + product.shipping;
+  }
+  let tax = parseFloat((price * 0.1).toFixed(2));
+  const grandTotal = price + shipping + tax;
   return (
-    <div>
+    <div className='container'>
       <h3 className="order-summary">Order Summary</h3>
         <div className="cart-info">
           <p>Selected Items: {cart.length}</p> 
-          <p>Total Price: </p>
-          <p>Total Shipping Charge: </p>
-          <p>Tax: </p>
-          <h4>Grand Total: </h4>
+          <p>Total Price: ${price}</p>
+          <p>Total Shipping Charge: ${shipping}</p>
+          <p>Tax: ${tax}</p>
+          <h4>Grand Total: ${grandTotal}</h4>
         </div>
         <button className="clear-cart-btn">
           <p className="cart-text">Clear Cart</p>
